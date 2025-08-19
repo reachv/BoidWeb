5c35a5b791c5eae9f8a0297c17e44536451faba8
reachv <reach.vann@gmail.com>
2025-08-19 10:57:40 -0700

    Aesthetic v5.1

--- src/PageComponents/PageConfig.tsx
+++ src/PageComponents/PageConfig.tsx
@@ -2,8 +2,12 @@
 export const PAGE_CONFIG = {
     COLOR_PALETTE:{
         CONTAINER_COLOR: "rgba(22, 22, 22, 1)",
-        TEXT_COLOR: "#0A192F"
+        TEXT_COLOR: "#0A192F",
+        LIST_GROUP_ITEM_BACKGROUND: "#112240",
+        LIST_GROUP_ITEM_TEXT: "#CCD6F6",
+
     },
+
     PADDING:{
         CONTAINER_PADDING: {
             TOP_PADDING: "4em",
@@ -14,6 +18,8 @@ export const PAGE_CONFIG = {
             TOP: "3em",
             BOTTOM: "2em"
         },
+
         SUBTEXT_PADDING:"1em",
+
     }
 }
